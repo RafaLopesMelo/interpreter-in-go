4a7b33c407bbbb0321448e667426a3d3b817ed8b
Rafael Melo <rafaelmelo.programmer@gmail.com>
2024-08-15 18:32:24 -0300

    chore: implement repl

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/RafaLopesMelo/monkey-lang/internal/repl"
+)
+
 func main() {
-	println("Hello, world!")
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello, %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
